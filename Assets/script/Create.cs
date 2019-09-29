330c21aa681edca9229d1ed9864268569e77b3cc
nekokaido <zeakradion@gmail.com>
2019-09-29 15:23:37 +0900

    ねこちゃんが図鑑上に映るよう変更｡

--- Assets/script/Create.cs
+++ Assets/script/Create.cs
@@ -8,10 +8,12 @@ public class Create : MonoBehaviour
     public Button button;
     private void Start()
     {
-        for (int i = 1; i <= 30; i++)
+        for (int i = 1; i <= 35; i++)
         {
             var obj = Instantiate(button, contentRectTransform);
             obj.GetComponentInChildren<Text>().text = i.ToString();
+            //if (i == 1)
+            //    obj.GetComponentInChildren<Text>().text = "neko";
         }
     }
 }

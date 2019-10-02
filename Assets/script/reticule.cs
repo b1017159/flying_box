f0658173e93c0fa7028c37b8a2dfff64bdf01286
b1017159 <b1017159@fun.ac.jp>
2019-10-02 15:13:27 +0900

    レティクル追加

---
+++ Assets/script/reticule.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reticule : MonoBehaviour
+{
+        private Vector3 replayer_position;
+        private Vector3 replayer_forward;
+        private Vector3 replayer_rotate;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+                replayer_position=Player.m_instance.transform.position;//自機のポジション
+                replayer_forward=Player.m_instance.transform.forward;//自機ののベクトル
+                replayer_rotate=Player.m_instance.transform.localEulerAngles;//自機の傾き
+                var retipos =replayer_position+replayer_forward*4;
+                //Debug.Log();
+                //transform.localPosition =new Vector3(0f, 0f, 0f);
+                transform.position =retipos;
+                transform.localEulerAngles=replayer_rotate;
+        }
+}

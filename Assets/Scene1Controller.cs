ec825f710164becd9fd4bc9a50f01042396ca15e
TAIRIKUTA <tairikuta2@gamil.com>
2019-10-09 16:56:10 +0900

    タイトルボタン

---
+++ Assets/Scene1Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//ボタンを使用するためUIとSceneManagerを使用ためSceneManagementを追加
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene1Controller : MonoBehaviour
+{
+
+    // ボタンをクリックするとBattleSceneに移動します
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("GameStage1");
+    }
+}

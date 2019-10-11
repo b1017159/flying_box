d9bda22348a181d3822394973db83ea8157270be
b1017159 <b1017159@fun.ac.jp>
2019-10-11 17:35:05 +0900

    import file

---
+++ Assets/script/Line.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+            LineRenderer Line = gameObject.GetComponent<LineRenderer>();
+            // 線の幅
+            Line.SetWidth(0.1f, 0.1f);
+            // 頂点の数
+            Line.SetVertexCount(2);
+            // 頂点を設定
+            Line.SetPosition(0, new Vector3(5.3f,-3f,0f));
+            Line.SetPosition(1, new Vector3(6.8f, -3.5f, 0f));
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/ShowScore.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ShowScore : MonoBehaviour
+{
+    private TextMesh Result;
+
+    int score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Result = GetComponentInChildren<TextMesh>();
+
+        score = Player.GetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        Result.text ="あなたのスコア　"+score+"m";
+
+        Scene loadscene = SceneManager.GetActiveScene();
+        if (OVRInput.Get(OVRInput.RawButton.A)) 
+        {
+                SceneManager.LoadScene("GameStage1");   
+        }
+        if (OVRInput.Get(OVRInput.RawButton.B))
+        {
+            SceneManager.LoadScene("GameTitle");
+        }
+
+
+    }
+
+}

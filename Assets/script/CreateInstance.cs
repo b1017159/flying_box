cf6af1c231471bfb88d71ea7e6bf6382309d46b1
nekokaido <zeakradion@gmail.com>
2019-10-18 14:43:13 +0900

    細かい変更

--- Assets/script/CreateInstance.cs
+++ Assets/script/CreateInstance.cs
@@ -9,14 +9,19 @@ public class CreateInstance : MonoBehaviour
     [SerializeField]
     private GameObject item;
 
+    private Dictionary<int, Transform> childObjects = new Dictionary<int, Transform>(16);
     public int num_item = 5;
+    
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < num_item; i++)
         {
             GameObject obj;
-            obj = Instantiate(item, new Vector3(0, 0, 0), Quaternion.identity, parent) as GameObject;    
+            obj = Instantiate(item, new Vector3(0, 0, 0), Quaternion.identity, parent) as GameObject;
+            obj.gameObject.name += i.ToString();
+            childObjects.Add(i, obj.transform);
+            Debug.Log(childObjects[i].gameObject.name);
             obj.transform.Rotate(0.0f, i*48.0f, 0.0f);
             if(i >= 5)
             {
@@ -24,10 +29,11 @@ public class CreateInstance : MonoBehaviour
             }
         }
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
+    //getterメソッド
+    public Dictionary<int, Transform> getChildren() { return this.childObjects; }
 }
--- Assets/script/ShowItems.cs
+++ Assets/script/ShowItems.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 public class ShowItems : MonoBehaviour
 {
     [SerializeField] private GameObject parentObject;
-    private List<Transform> childObjects = new List<Transform>(12);
 
     [SerializeField] public float wheelSpeed = 1f;
     public float rotatespeed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        //はじめに子オブジェクトをすべて取得する｡
-        foreach(Transform childTranstorm in parentObject.transform)
-        {
-            childObjects.Add(childTranstorm);
-        }
-        foreach(Transform child in childObjects)
-        {
-            Debug.Log(child.gameObject.name);
-        }
+
     }
 
     // Update is called once per frame
@@ -28,7 +19,7 @@ public class ShowItems : MonoBehaviour
     {
         this.transform.Rotate(new Vector3(0.0f, rotatespeed * Time.deltaTime, 0.0f));
         float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
-        if(scrollWheel != 0.0f)
+        if(System.Math.Abs(scrollWheel) > 0)
         {
             MouseWheel(scrollWheel);
         }

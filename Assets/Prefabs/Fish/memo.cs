7540b049f6a59234268ba168ebe2aeac9532387f
b1017159 <b1017159@fun.ac.jp>
2019-11-06 16:11:50 +0900

    追いかける魚がランダムに生成されるように作成 prehabに魚の大きさを記載するC#スクリプトを作成

---
+++ Assets/Prefabs/Fish/memo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class memo : MonoBehaviour
+{
+        // Start is called before the first frame update
+        void Start()
+        {
+                /*
+                   名前　scale min_scale
+                   アイナメ 6 5
+                   アメマス 6 5
+                   ベロ 2 1
+                   ダイナンギンポ 3 2
+                   ドロメ 6 3
+                   イトウ 10 5
+                   エゾメバル 6 3
+                   イシダイ 10 7
+                   イトウ 10 6
+                   ニシンの群れ 10 10
+                   シマゾイ 4 3
+                   ウミタナゴ 2 1
+                 */
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+}

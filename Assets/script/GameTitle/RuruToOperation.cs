725f8ccd5153beefebf28f437f1ccec4cf9e95aa
TAIRIKUTA <tairikuta2@gamil.com>
2019-11-06 15:25:37 +0900

    チュートリアル画面の作成・名前入力修正

---
+++ Assets/script/GameTitle/RuruToOperation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RuruToOperation : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene("Operation");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            SceneManager.LoadScene("Operation");
+        }
+    }
+}
--- Assets/script/GameTitle/SpotLightOnOff.cs
+++ Assets/script/GameTitle/SpotLightOnOff.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering;
 
 public class SpotLightOnOff : Toggle_Day
 {
+
+
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         int A = Toggle_Day.GetOneDay();
+       
 
         if (A == 2)
         {
+
+
             //太陽光見えなくする
             GameObject obj = GameObject.Find("Directional Light");
             obj.SetActive(false);
@@ -19,10 +24,14 @@ public class SpotLightOnOff : Toggle_Day
             //太陽光削除
             GameObject obj3 = GameObject.Find("SunLight");
             Destroy(obj3);
+
+          
+            //環境光黒色
+            RenderSettings.ambientLight = Color.black;
+
         }
         else
         {
-        
                 GameObject obj4 = GameObject.Find("Spot Light");
                 obj4.SetActive(false);
             }

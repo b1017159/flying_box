3f33a639f23357ab1851e1cd78d1cb84902e1e80
宮下翔伍 <syogo0323sekaowa@docomo.ne.jp>
2019-11-12 18:56:01 +0900

    PC版のゲームシーンのエラー解消、テキストの大きさ、修正完了

--- Assets/script/GameScene/OptionController.cs
+++ Assets/script/GameScene/OptionController.cs
@@ -19,17 +19,12 @@ public class OptionController : MonoBehaviour
     public GameObject CameraText_3_non; //非選択時のTPSの文字表示
     public GameObject CameraText_1_non; //非選択時のTPSの文字表示
     public GameObject sound;
+    public GameObject YesTitle;
+    public GameObject menuText;
     private GameObject FPS;  //一人称カメラ
     private int changemenu; //メニューを切り替えるための変数
     private int mode; //オプションとゲームシーンを切り替え
-    private GameObject TextObject1;
-    private GameObject TextObject2;
-    private GameObject TextObject3;
-    private GameObject TextObject4;
-    private GameObject TextObject5;
-    private GameObject TextObject6;
-    private GameObject TextObject7;
-    private GameObject TextObject8;
+   
     // Start is called before the first frame update
 
     void Start()
@@ -39,34 +34,19 @@ public class OptionController : MonoBehaviour
         FPS = GameObject.Find("One_person");
         if (FPS.activeSelf)
         {
-            CameraText_1.SetActive(true);
+            CameraText_1.SetActive(false);
             CameraText_1_non.SetActive(true);
-            CameraText_3.SetActive(false);
-            CameraText_3_non.SetActive(false);
-
-            Camerasig = 1;
+            CameraText_3.SetActive(true);
+            CameraText_3_non.SetActive(false);  
         }
         else
         {
-            CameraText_1.SetActive(false);
+            CameraText_1.SetActive(true);
             CameraText_1_non.SetActive(false);
-            CameraText_3.SetActive(true);
+            CameraText_3.SetActive(false);
             CameraText_3_non.SetActive(true);
-
-            Camerasig = 3;
         }
-        TextObject5 = GameObject.Find("TextObject1");
-        TextObject6 = GameObject.Find("YesTitle1");
-        GameObject obj2 = GameObject.Find("One_OptionMenu");
-        TextObject7 = obj2.transform.Find("TextObject1").gameObject;
-        TextObject8 = obj2.transform.Find("YesTitle1").gameObject;
-
-        TextObject1 = GameObject.Find("TextObject3");
-        TextObject3 = GameObject.Find("YesTitle3");
-        GameObject obj = GameObject.Find("Third_OptionMenu");
-        TextObject2 = obj.transform.Find("TextObject3").gameObject;
-        TextObject4 = obj.transform.Find("YesTitle3").gameObject;
-
+       
     }
 
     // Update is called once per frame
@@ -113,8 +93,10 @@ public class OptionController : MonoBehaviour
             checkbox_backgame.SetActive(false);
             checkbox_yes.SetActive(false);
             checkbox_no.SetActive(false);
+            YesTitle.SetActive(false);
             if (Input.GetKeyDown(KeyCode.Space)) //カメラの切り替えを行う
             {
+                Debug.Log(Camerasig);
                 if (Camerasig == 1)
                 {
                     CameraText_1.SetActive(false);
@@ -129,14 +111,15 @@ public class OptionController : MonoBehaviour
                     CameraText_1_non.SetActive(false);
                     CameraText_3.SetActive(false);
                     CameraText_3_non.SetActive(true);
+                   
+                    
                     Camerasig = 1;
                 }
             }
         }
         if (changemenu == 1)
         {
-            TextObject3.SetActive(false);
-            TextObject6.SetActive(false);
+           
             checkbox_camera.SetActive(false);
             checkbox_sound.SetActive(true);
             checkbox_gotitle.SetActive(false);
@@ -155,18 +138,16 @@ public class OptionController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 changemenu = 5;
+                YesTitle.SetActive(true);
+                menuText.SetActive(false);
 
-                TextObject1.SetActive(false);
-                TextObject4.SetActive(true);
-                TextObject5.SetActive(false);
-                TextObject8.SetActive(true);
+               
             }
         }
 
         if (changemenu == 3)
         {
-            TextObject3.SetActive(false);
-            TextObject6.SetActive(false);
+            
             checkbox_camera.SetActive(false);
             checkbox_sound.SetActive(false);
             checkbox_gotitle.SetActive(false);
@@ -202,6 +183,7 @@ public class OptionController : MonoBehaviour
             checkbox_sound.SetActive(false);
             checkbox_gotitle.SetActive(false);
             checkbox_backgame.SetActive(false);
+           
             if (Input.GetKey(KeyCode.Y)) //カメラの切り替えを行う
             {
                 SceneManager.LoadScene("GameTitle");
@@ -219,11 +201,17 @@ public class OptionController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.N)) //カメラの切り替えを行う
             {
                 changemenu = 2;
+                YesTitle.SetActive(false);
+                checkbox_camera.SetActive(false);
+                checkbox_sound.SetActive(false);
+                checkbox_gotitle.SetActive(true);
+                checkbox_backgame.SetActive(false);
+                checkbox_yes.SetActive(false);
+                checkbox_no.SetActive(false);
+                menuText.SetActive(true);
 
-                TextObject2.SetActive(true);
-                TextObject3.SetActive(false);
-                TextObject6.SetActive(false);
-                TextObject7.SetActive(true);
+
+
 
             }
         }

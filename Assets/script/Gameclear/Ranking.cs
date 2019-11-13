d6652a5d268490e749e6b702542880bd85539036
b1017159 <b1017159@fun.ac.jp>
2019-11-13 13:43:05 +0900

    ランキング機能導入

---
+++ Assets/script/Gameclear/Ranking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranking : MonoBehaviour
+{
+        // Start is called before the first frame update
+        void Start()
+        {
+                naichilab.RankingLoader.Instance.SendScoreAndShowRanking (100);
+                //100を魚の大きさにする
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+}

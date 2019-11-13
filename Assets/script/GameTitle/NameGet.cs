98df6d836b32a0408487b5cb066bb1787f07c68e
TAIRIKUTA <tairikuta2@gamil.com>
2019-11-13 14:37:21 +0900

    タイトル画面の文字調節

--- Assets/script/GameTitle/NameGet.cs
+++ Assets/script/GameTitle/NameGet.cs
@@ -13,7 +13,7 @@ public class NameGet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        username = InputFieldManager.GetName();
+        username = InputName.GetName();
 
         this.targetText = this.GetComponent<Text>();
 

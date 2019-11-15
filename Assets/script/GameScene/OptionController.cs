2b9b8de41b693a882d17d43872df05c9905ac194
b1017121 <b1017121@fun.ac.jp>
2019-11-15 16:09:58 +0900

    ゲームシーンのオプションの調整

--- Assets/script/GameScene/OptionController.cs
+++ Assets/script/GameScene/OptionController.cs
@@ -12,11 +12,16 @@ public class OptionController : MonoBehaviour
     public GameObject checkbox_sound;
     public GameObject checkbox_gotitle;
     public GameObject checkbox_backgame;
+    public GameObject checkbox_yes;
+    public GameObject checkbox_no;
     public GameObject CameraText_3; //選択時のTPSの文字表示
     public GameObject CameraText_1; //選択時のFPSの文字表示
     public GameObject CameraText_3_non; //非選択時のTPSの文字表示
     public GameObject CameraText_1_non; //非選択時のTPSの文字表示
     public GameObject sound;
+    public GameObject YesTitle;
+    public GameObject menuText;
+
     private int changemenu; //メニューを切り替えるための変数
     private int mode; //オプションとゲームシーンを切り替え
     // Start is called before the first frame update
@@ -47,16 +52,32 @@ public class OptionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        if (changemenu != 5 && changemenu != 9 && OVRInput.GetDown(OVRInput.RawButton.X))
 
-        if (OVRInput.GetDown(OVRInput.RawButton.LThumbstickDown))
+        {
+            mode = 1;
+            changemenu = 0;
+            Player.ControllSwitch = 0;
+            Time.timeScale = 0;
+        }
+        
+        if (changemenu != 5 && changemenu !=9 && OVRInput.GetDown(OVRInput.RawButton.LThumbstickDown))
         {
             changemenu += 1;
         }
-
-        if (OVRInput.GetDown(OVRInput.RawButton.LThumbstickUp))
+        if (changemenu != 5 && changemenu !=9 && OVRInput.GetDown(OVRInput.RawButton.LThumbstickUp))
         {
             changemenu -= 1;
-        } 
+        }
+        if (changemenu == 5 && OVRInput.GetDown(OVRInput.RawButton.LThumbstickRight))
+        {
+            changemenu += 4;
+        }
+        if (changemenu == 9 && OVRInput.GetDown(OVRInput.RawButton.LThumbstickLeft))
+        {
+            changemenu -= 4;
+        }
 
         //メニューごとに実行内容を変更
         if (changemenu == -1)
@@ -69,7 +90,10 @@ public class OptionController : MonoBehaviour
             checkbox_sound.SetActive(false);
             checkbox_gotitle.SetActive(false);
             checkbox_backgame.SetActive(false);
-            if (OVRInput.GetDown(OVRInput.RawButton.A)) //カメラの切り替えを行う
+            checkbox_yes.SetActive(false);
+            checkbox_no.SetActive(false);
+            YesTitle.SetActive(false);
+                if (OVRInput.GetDown(OVRInput.RawButton.A)) //カメラの切り替えを行う
             {
                 if (Camerasig == 1)
                 {
@@ -95,6 +119,8 @@ public class OptionController : MonoBehaviour
             checkbox_sound.SetActive(true);
             checkbox_gotitle.SetActive(false);
             checkbox_backgame.SetActive(false);
+            checkbox_yes.SetActive(false);
+            checkbox_no.SetActive(false);
         }
         if (changemenu == 2)
         {
@@ -102,20 +128,30 @@ public class OptionController : MonoBehaviour
             checkbox_sound.SetActive(false);
             checkbox_gotitle.SetActive(true);
             checkbox_backgame.SetActive(false);
-           /* if (OVRInput.GetDown(OVRInput.RawButton.A))
+            checkbox_yes.SetActive(false);
+            checkbox_no.SetActive(false);
+                if (OVRInput.GetDown(OVRInput.RawButton.A))
             {
-                SceneManager.LoadScene("GameTitle");
-            }*/
+                changemenu = 5;
+                YesTitle.SetActive(true);
+                menuText.SetActive(false);
+            }
+            /* if (OVRInput.GetDown(OVRInput.RawButton.A))
+             {
+                 SceneManager.LoadScene("GameTitle");
+             }*/
         }
+
         if (changemenu == 3)
         {
             checkbox_camera.SetActive(false);
             checkbox_sound.SetActive(false);
             checkbox_gotitle.SetActive(false);
             checkbox_backgame.SetActive(true);
+            checkbox_yes.SetActive(false);
+            checkbox_no.SetActive(false);
 
-
-            if (OVRInput.GetDown(OVRInput.RawButton.A))
+                if (OVRInput.GetDown(OVRInput.RawButton.A))
             {
                 if(Camerasig == 1)
                 {
@@ -137,17 +173,55 @@ public class OptionController : MonoBehaviour
 
         //Debug.Log(changemenu);
 
+        if (changemenu == 5)
+        {
+            checkbox_yes.SetActive(true);
+            checkbox_no.SetActive(false);
+            checkbox_camera.SetActive(false);
+            checkbox_sound.SetActive(false);
+            checkbox_gotitle.SetActive(false);
+            checkbox_backgame.SetActive(false);
+                if (OVRInput.GetDown(OVRInput.RawButton.A)) //カメラの切り替えを行う
+            {
+                SceneManager.LoadScene("GameTitle");
+            }
+        }
+
+        if (changemenu == 9)
+        {
+            checkbox_yes.SetActive(false);
+            checkbox_no.SetActive(true);
+            checkbox_camera.SetActive(false);
+            checkbox_sound.SetActive(false);
+            checkbox_gotitle.SetActive(false);
+            checkbox_backgame.SetActive(false);
+                if (OVRInput.GetDown(OVRInput.RawButton.B))//カメラの切り替えを行う
+            {
+                changemenu = 2;
+                YesTitle.SetActive(false);
+                checkbox_camera.SetActive(false);
+                checkbox_sound.SetActive(false);
+                checkbox_gotitle.SetActive(true);
+                checkbox_backgame.SetActive(false);
+                checkbox_yes.SetActive(false);
+                checkbox_no.SetActive(false);
+                menuText.SetActive(true);
 
 
 
-        if (OVRInput.GetDown(OVRInput.RawButton.X))
+
+            }
+        }
+
+
+       /* if (OVRInput.GetDown(OVRInput.RawButton.X))
         {
             mode = 1;
             changemenu = 0;
             Player.ControllSwitch = 0;
             Time.timeScale = 0;
         }
-       /* if (Input.GetKey(KeyCode.P))
+        if (Input.GetKey(KeyCode.P))
         {
             mode = 0;
             Player.ControllSwitch = 1;

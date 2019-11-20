d63a4fcd4297185b93cab5312c62a50dfa868556
b1017121 <b1017121@fun.ac.jp>
2019-11-20 17:10:08 +0900

    真モデルファイルの魚に変更、エネミーマネージャーの調整

--- Assets/Prefabs/Fish/memo.cs
+++ Assets/Prefabs/Fish/memo.cs
@@ -7,32 +7,32 @@ public class memo : MonoBehaviour
         // Start is called before the first frame update
         void Start()
         {
-                /*
-                   名前　scale min_scale
-                   アイナメ 6 5
-                   アメマス 6 5
-                   ベロ 2 1
-                   ダイナンギンポ 3 2
-                   ドロメ 6 3
-                   イトウ 10 5
-                   エゾメバル 6 3
-                   ゴマギンポ 3 2
-                   ヒガンフグ 4 2
-                   イシダイ 10 7
-                   カワハギ 3 2
-                   マンボウ 25 10
-                   ニシン 4 2
-                   オウゴンムラソイ 3 2
-                   マフグ 5 3
-                   イトウ 10 6
-                   ニシンの群れ 10 10
-                   シマゾイ 4 3
-                   ウミタナゴ 2 1
-                 */
-        }
+        /*
+           名前　scale min_scale
+           (常に2)ニシンの群れ 10 10
+           3マンボウ 25 10 
+           3イトウ 10 5
+           3イシダイ 10 7  
+           2アイナメ 6 5
+           2アメマス 7 5   
+           2ホッケ 5 3
+           2マフグ 5 3
+           2シマゾイ 4 3
+           2ヒガンフグ 4 2
+           2ダイナンギンポ 4 2
+           1ゴマギンポ 3 2
+           1エゾメバル 3 2
+           1オウゴンムラソイ 3 2
+           1カワハギ 3 1
+           1ウミタナゴ 3 1
+           1ベロ 2 1
+           1ドロメ 2 1 
+           
+         */
+    }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
         {
 
         }

c0c5cff73bfe5a1b64cc36470125eb73431cb5d9
宮下翔伍 <syogo0323sekaowa@docomo.ne.jp>
2019-11-22 01:17:29 +0900

    ゲームシーンの光のエラーの修正、および、ゲームタイトル、チュートリアル、ゲームシーンのオプション時にサウンドを追加

--- Assets/script/GameTitle/OperationToStage1.cs
+++ Assets/script/GameTitle/OperationToStage1.cs
@@ -5,12 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class OperationToStage1 : MonoBehaviour
 {
+    public AudioClip SoundEffect;
+    AudioSource audioSource;
+    private void Awake()
+    {
+        int DestroyCheck = FindObjectsOfType<OperationToStage1>().Length;
+        if (DestroyCheck > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        Scene loadscene = SceneManager.GetActiveScene();
+        if (loadscene.name == "Operation")
         {
-            SceneManager.LoadScene("GameStage1");
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                audioSource.PlayOneShot(SoundEffect);
+                SceneManager.LoadScene("GameStage1");
+            }
+            
         }
-
     }
+   
+       
+
+   
 }
--- Assets/script/GameTitle/RuruBack.cs
+++ Assets/script/GameTitle/RuruBack.cs
@@ -1,16 +1,15 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
-public class TitleSceneManager : MonoBehaviour
+
+public class RuruBack : MonoBehaviour
 {
     public AudioClip SoundEffect;
     AudioSource audioSource;
     private void Awake()
     {
-        int DestroyCheck = FindObjectsOfType<TitleSceneManager>().Length;
+        int DestroyCheck = FindObjectsOfType<RuruBack>().Length;
         if (DestroyCheck > 1)
         {
             Destroy(gameObject);
@@ -27,24 +26,15 @@ public class TitleSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
         Scene loadscene = SceneManager.GetActiveScene();
-        if (loadscene.name == "GameTitle")
+        if (loadscene.name == "Operation")
         {
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.B))
             {
                 audioSource.PlayOneShot(SoundEffect);
-                SceneManager.LoadScene("Name");
+                SceneManager.LoadScene("Ruru");
             }
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                audioSource.PlayOneShot(SoundEffect);
-                SceneManager.LoadScene("TopRanking");
-            }
+
         }
-     
-
     }
-   
-
 }

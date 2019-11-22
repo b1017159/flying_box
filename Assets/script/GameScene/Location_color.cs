cc20240aee7206e26cf3b77dd5526948f6460429
b1017159 <b1017159@fun.ac.jp>
2019-11-22 17:15:13 +0900

    上を向くと地名が黒くなる機能を導入

---
+++ Assets/script/GameScene/Location_color.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location_color : MonoBehaviour
+{
+        // Start is called before the first frame update
+        TextMesh text;
+        bool flag=true;
+        void Start()
+        {
+                text= this.GetComponent<TextMesh>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+                if(Player.updown<-15&&flag==true) {
+                        text.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                        flag=false;
+                }else if(Player.updown>-15&&flag==false) {
+                        text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                        flag=true;
+                }
+        }
+}
